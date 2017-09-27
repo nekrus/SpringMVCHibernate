374882d0c7e0760627a9f4048729a0c7766770e4
Damian Pawłowski <dam.pawlowski@gmail.com>
2017-09-27 10:49:49 +0200

    Initial commit

---
+++ TestSpringMVCHibernate/src/main/java/com/example/TestSpringMVCHibernate/TestSpringMvcHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.example.TestSpringMVCHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringMvcHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringMvcHibernateApplication.class, args);
+	}
+}
